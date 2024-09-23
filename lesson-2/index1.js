6d22880751bf1448148aea25d7e9834d77374e80
Alena Nemtsova <jelenatbs@gmail.com>
2024-09-23 23:18:49 +0300

    refactored

--- lesson-2/index1.js
+++ lesson-2/index1.js
@@ -5,23 +5,20 @@ Array.prototype.myFilter = function (callback, object) {
     const filteredArray = [];
 
     for (let i = 0; i < length; i++) {
-        const element = object[i];
-        const index = i;
-        const array = object;
-
-        const result = callback(element, index, array);
+        const result = callback(object[i], i, object);
 
         if (result) {
-            filteredArray.push(element);
+            filteredArray.push(object[i]);
         }
     }
+
     return filteredArray;
 }
 
 
 //tests
 const numbers = [12, 5, 8, 130, 44];
-const filteredNumbers = numbers.myFilter((value) => (value >= 10));
+const filteredNumbers = numbers.myFilter((value) => value >= 10);
 console.log(filteredNumbers);
 
 const words = ['spray', 'elite', 'exuberant', 'destruction', 'present'];
