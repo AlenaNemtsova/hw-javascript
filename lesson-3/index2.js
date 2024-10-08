4a604b2ec47f54722db2f85f7f0b517d612cff19
Alena Nemtsova <jelenatbs@gmail.com>
2024-10-08 15:08:16 +0300

    add other methods

--- lesson-3/index2.js
+++ lesson-3/index2.js
@@ -7,13 +7,19 @@ function getPersons(name, age) {
         }
     }
 
-    const obj2 = new MyObject(name, age);
+    const obj2 = new MyObject('name', age);
 
     const obj3 = Object.create(Object.prototype);
     obj3.name = name;
     obj3.age = age;
 
     const obj4 = Object.assign({}, { name, age });
+
+    const obj5 = { ...obj2 }
+
+    const obj6 = structuredClone(obj2)
+
+    const obj7 = JSON.parse(JSON.stringify(obj2))
 
     return [
         {
@@ -22,7 +28,10 @@ function getPersons(name, age) {
         },
         obj2,
         obj3,
-        obj4
+        obj4,
+        obj5,
+        obj6,
+        obj7
     ];
 }
 
